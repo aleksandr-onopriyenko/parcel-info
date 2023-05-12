95a00e1825d34324c9627aad0ab78ff5bfb00c5e
Aleksandr Onopriyenko <onopriyenkoalex@gmail.com>
2023-05-12 19:32:04 +0300

    feat: add exports from Address components

---
+++ src/components/Address/index.js
@@ -0,0 +1,5 @@
+export { Map } from './Map'
+export { MapInput } from './MapInput'
+export { Marker, MarkerIcon } from './Marker'
+export { MarkersHint } from './MarkersHint'
+export { LoaderMapLocation } from './LoaderMapLocation'
