c0fdb3b341dfce04317d7621d5f51a1d32492cbc
Aleksandr Onopriyenko <onopriyenkoalex@gmail.com>
2023-05-13 08:21:46 +0300

    feat: add baseUrl for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: 'https://aleksandr-onopriyenko.github.io/parcel-info/',
   resolve: {
     alias: {
       '@': resolve(__dirname, '/src')
